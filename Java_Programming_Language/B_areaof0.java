07c6dde5df702557d58df89d7f70b7463f9bf6cc
Shreyash Sature <shreyashsature@gmail.com>
2025-11-20 19:48:52 +0530

    Program to calculate Area of Circle

---
+++ Java_Programming_Language/B_areaof0.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class B_areaof0 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of circle : ");
+        int rad = sc.nextInt();
+        double area = rad* rad* 3.14;
+
+        System.out.print("Area of Circle is : ");
+        System.out.println(area);
+
+    }
+    
+}
