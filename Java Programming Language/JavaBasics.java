3abe9600a78ae3db1470f6ccb9fda967e3c91f8a
Shreyash Sature <shreyashsature@gmail.com>
2025-11-20 19:38:38 +0530

    First Java Basic Program

---
+++ Java Programming Language/JavaBasics.java
@@ -0,0 +1,30 @@
+public class JavaBasics{
+    public static void main(String[] args) {
+        System.out.println ("Hellow World!\n");
+        System.out.print("Hellow\n");
+       
+        
+    }
+    // public static void main(String[] args) {
+    //     byte a=8;
+    //     System.out.println(a);
+    //     char ch = 'b';
+    //     System.out.println(ch);
+    //     boolean var = true;
+    //     System.out.println(var);
+
+    //     double r = 12.3;
+    //     System.out.println(r);
+
+    //     int d=12;
+    //     System.out.println(d);
+
+
+
+    // }
+
+
+
+    
+    
+}
