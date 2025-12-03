2a01abd9c25664fa175adbef3eda8ace51646252
Shreyash Sature <shreyashsature@gmail.com>
2025-12-03 23:44:47 +0530

    update

--- Java_Programming_Language/05_Patterns/invpyr.java
+++ Java_Programming_Language/05_Patterns/invpyr.java
@@ -2,20 +2,19 @@ import java.util.*;
 public class invpyr {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Enter number of lines : ");
+        System.out.print("Enter number of lines to be printed : ");
         int n = sc.nextInt();
 
         for(int i = 1; i<=n; i++){
-            for(int j = 0; j<=n-i; i++){
+            for(int j = 0; j<=n-i; j++){
                 System.out.print("  ");
             }
             for(int k = 1; k<=i; k++){
                 System.out.print("* ");
                 
             }
-           System.out.println();
+            System.out.println("");
         }
         
-    }
-    
+    }  
 }
