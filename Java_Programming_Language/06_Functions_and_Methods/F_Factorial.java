fc60dd36579ce84bd95f59a3fe734bd3aa7aadcf
Shreyash Sature <shreyashsature@gmail.com>
2025-12-04 10:56:18 +0530

    Factorial

---
+++ Java_Programming_Language/06_Functions_and_Methods/F_Factorial.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class F_Factorial {
+
+    public static int factorial(int num){
+        int fact =1;
+        for (int i =1; i<=num;i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to find its factorial : ");
+        int num = sc.nextInt();
+
+        int fact =factorial(num);
+        System.out.println("Factorial of given number is : "+fact);
+
+
+
+        
+    }
+    
+}
