374235aa2ae51c8ca335b7d71571fb7e456da578
Shreyash Sature <shreyashsature@gmail.com>
2025-12-04 09:55:56 +0530

    update

---
+++ Java_Programming_Language/06_Functions_and_Methods/A_FunctionSyntax.java
@@ -0,0 +1,23 @@
+/*
+
+returnType func_name(){
+
+     //body
+
+     return statement;
+}
+
+In java all functions are written in the class & the functions written in class are generally called as methods.
+*/
+public class A_FunctionSyntax{
+
+    public static void HelloWorld(){
+
+        System.out.println("Hellow World");
+    }
+
+    public static void main(String[] args) {
+
+        HelloWorld(); // function call
+    }
+}
