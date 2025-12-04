d57a0cc6fcb70e2a5b5df156002fd0fcb2763d16
Shreyash Sature <shreyashsature@gmail.com>
2025-12-04 18:44:47 +0530

    Inbuilt functions

---
+++ Java_Programming_Language/06_Functions_and_Methods/H_Inbuilt_and_UserDefined_Methods.java
@@ -0,0 +1,29 @@
+/*
+| Area                | Class     | Method (Inbuilt Function)  | Purpose                    |
+| ------------------- | --------- | -------------------------- | -------------------------- |
+| **Math**            | `Math`    | `sqrt(x)`                  | Square root                |
+|                     |           | `pow(a, b)`                | (a^b)                      |
+|                     |           | `max(a, b)`                | Greatest of two values     |
+|                     |           | `abs(x)`                   | Absolute value             |
+
+| **String Handling** | `String`  | `length()`                 | Count characters           |
+|                     |           | `charAt(i)`                | Get character at index     |
+|                     |           | `toLowerCase()`            | Convert to lowercase       |
+|                     |           | `equals(str)`              | Compare two strings        |
+
+| **Array Handling**  | `Arrays`  | `sort(array)`              | Sort array                 |
+|                     |           | `fill(array, value)`       | Fill with given value      |
+|                     |           | `binarySearch(array, key)` | Search key in sorted array |
+
+| **Input**           | `Scanner` | `nextInt()`, `nextLine()`  | Read input from user       |
+
+| **Conversion**      | `Integer` | `parseInt("123")`          | Convert String → int       |
+|                     | `String`  | `valueOf(int)`             | Convert int → String       |
+
+| **System**          | `System`  | `currentTimeMillis()`      | Current time in ms         |
+|                     |           | `exit(0)`                  | Stop program               |
+ */
+
+public class H_Inbuilt_and_UserDefined_Methods {
+    
+}
