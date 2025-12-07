74bb18a9e63503d78b265ab3e62638c4849fbe9c
Shreyash Sature <shreyashsature@gmail.com>
2025-12-07 14:17:03 +0530

    Operations on Arrays : Input, Output, Update

---
+++ B_Arrays/A_Array_Creation.java
@@ -0,0 +1,25 @@
+
+/*
+Arrays : List of elements of same type placed in contegious memory locations.
+         An array is a data structure used to store multiple values of the same type in a single variable,
+         instead of declaring separate variables.
+
+Syntax :
+
+dataType arrayName [] = new dataType[Size]; 
+
+*/
+
+public class A_Array_Creation {
+    public static void main(String[] args) {
+        
+        int arr[] = new int[4];
+        arr[0] =1;
+        arr[1]=2;
+
+        int numbers[] = {1,2,3,4};
+
+        String Fruits[] = {"Mango", "Apple", "Orange"};
+    }
+    
+}
---
+++ B_Input_Output_Update_ofArray.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class B_Input_Output_Update_ofArray {
+    public static void Input_Arr(int marks[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enters marks of English :");
+        marks[0]= sc.nextInt();
+        System.out.print("Enters marks of Physics :");
+        marks[1]= sc.nextInt();
+        System.out.print("Enters marks of Maths :");
+        marks[2]= sc.nextInt();
+    }
+
+    public static void Output_Arr(int marks[]){
+        System.out.println("English Marks : "+ marks[0]);
+        System.out.println("Physics Marks : "+ marks[1]);
+        System.out.println("Maths Marks : "+ marks[2]);
+
+    }
+    public static void Update_Arr(int marks[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enters updated marks of English :");
+        marks[0]=sc.nextInt();
+        System.out.println("Updated English Marks : "+ marks[0]);
+
+    }
+
+    public static void main(String[] args) {
+        int marks[]=new int[5];
+
+        Input_Arr(marks);
+        Output_Arr(marks);
+        Update_Arr(marks);
+
+
+    }
+    
+}
