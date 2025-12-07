a502df8dd9a5880968eb8d4136f9033d9c3c83c0
Shreyash Sature <shreyashsature@gmail.com>
2025-12-07 12:51:36 +0530

    Hollow Rectangle

---
+++ Java_Programming_Language/07_Patterns_Advanced/A_Hollow_Rectangle.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class A_Hollow_Rectangle {
+
+    public static void Hollow_Rec(int rows, int cols){
+        for (int i=0; i<rows;i++){
+            for(int j=0; j<cols;j++){
+                if(i==0|| i==rows-1 || j==0|| j==cols-1){
+
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of columns :");
+        int cols = sc.nextInt();
+        System.out.print("Enter number of rows :");
+        int rows = sc.nextInt();
+
+        
+    }
+    
+}
