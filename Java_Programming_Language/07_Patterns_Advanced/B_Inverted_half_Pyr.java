f22255e37f64467fcfd6e1cc94b0d365f7c801ef
Shreyash Sature <shreyashsature@gmail.com>
2025-12-07 13:10:48 +0530

    Inverted Pyramid

--- Java_Programming_Language/07_Patterns_Advanced/B_Inverted_half_Pyr.java
+++ Java_Programming_Language/07_Patterns_Advanced/B_Inverted_half_Pyr.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class B_Inverted_half_Pyr {
     public static void Pyramid(int num){
         for(int i=1;i<num;i++){
-            for(int j=1;j<=num-i;j++){
+            for(int j=1;j<=num-i;j++){   //Loop for spaces
                 System.out.print("  ");
             }
-            for(int k=1; k<=i;k++){
+            for(int k=1; k<=i;k++){      // Loop for Stars
                 System.out.print("* ");
             }
             System.out.println("");
@@ -22,3 +22,12 @@ public class B_Inverted_half_Pyr {
     }
     
 }
+
+/*
+Output : Enter value of num : 6
+          * 
+        * * 
+      * * * 
+    * * * * 
+  * * * * * 
+*/
