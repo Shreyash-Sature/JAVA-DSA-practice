622966a228cde71bcd25293fae9f4dea0b1aef72
Shreyash Sature <shreyashsature@gmail.com>
2026-01-26 01:44:15 +0530

    Sum of n natural numbers

---
+++ H_Recursion/D_Sum_of_n_natural_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class D_Sum_of_n_natural_numbers {
+    public static int Sum(int n){
+        if (n==1){
+            return 1;
+        }
+        int Sum = n + Sum(n-1);
+        return Sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(Sum(n));
+
+    }
+    
+}
