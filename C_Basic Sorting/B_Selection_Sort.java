6975a6b0535fae0ef5219d3d1d426502f68e5238
Shreyash Sature <shreyashsature@gmail.com>
2026-01-29 19:33:04 +0530

    Selection Sort

---
+++ C_Basic Sorting/B_Selection_Sort.java
@@ -0,0 +1,26 @@
+public class B_Selection_Sort {
+    public static void SelectionSort(int arr[]){
+        
+        for(int i =0;i<arr.length-1;i++){
+            int small = Integer.MAX_VALUE;
+            for(int j =i;j<arr.length;j++){
+                
+                if(arr[j]<small){
+                    small = arr[j];
+                    
+                    int temp = arr[i];
+                    arr[i]=small;
+                    arr[j]=temp;
+                }
+                
+            }
+        }
+        for(int i =0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,2,8,3,9};
+        SelectionSort(arr);
+    }
+}
