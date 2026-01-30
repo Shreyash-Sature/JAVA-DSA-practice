1efcf7fbcab7ac763a6dda5f8cf6e6acef0f9362
Shreyash Sature <shreyashsature@gmail.com>
2026-01-30 17:59:16 +0530

    Binary to decimal

---
+++ A_Java_Programming_Language/06_Functions_and_Methods/BiDeci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BiDeci {
+
+    public static double BiToDeci(double Binary){
+        double Deci = 0;
+        while(Binary >=0){
+            
+        }
+        return Deci;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double Binary = sc.nextDouble();
+
+
+    }
+    
+}
