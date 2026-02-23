2ddaf43cad7cd3aee6ca40d7368f33245a812ec8
Shreyash Sature <shreyashsature@gmail.com>
2026-02-23 20:01:39 +0530

    Last Occurance of eleme nt in array

---
+++ H_Recursion/H_Last_Occurance.java
@@ -0,0 +1,16 @@
+public class H_Last_Occurance {
+    public static int LastOccurance(int arr [], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound = LastOccurance(arr, key, i+1);
+        if(isFound == -1 && arr[i]==key){
+            return i;
+        }
+        return isFound;
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,2,1,5,7,3,9};
+        System.out.println(LastOccurance(arr, 3, 0)); 
+    }
+}
